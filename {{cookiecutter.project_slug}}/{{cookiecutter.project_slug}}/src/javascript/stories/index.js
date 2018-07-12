aba189b73ce9f82b51bf39b15518997474a6efd7
Mona Lisa <MonaLisa@faker.com>
2018-07-11 22:33:27 -0400

    Got cookiecutter working again

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/src/javascript/stories/index.js
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/src/javascript/stories/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import Button from '@material-ui/core/Button';
+
+// Get our random number story
 import randomNumberStory from './randomNumber';
 
+
+
 storiesOf('Button', module)
-  .add('with text', randomNumberStory);   
+  .add('with text', () => (
+    <Button
+    variant="contained"
+    color="primary"
+    onClick={action('clicked')}
+    >
+        Hello Button
+    </Button>
+  ))
+  .add('with some emoji', () => (
+    <Button
+    variant="contained"
+    color="primary"
+    onClick={action('clicked')}
+    >
+        <span role="img" aria-label="so cool">😀 😎 👍 💯</span>
+    </Button>
+  ));   
